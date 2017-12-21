5ccd23bd43595ce912f7b80639d7d53c5d532206
jpx <pengxiang@momenta.ai>
2017-12-22 00:11:09 +0800

    model identification

--- model_identification/model_test.m
+++ model_identification/model_test.m
@@ -3,8 +3,8 @@ function model_test
 addpath('../lib');
 
 dt = 0.01;
-iir_index1 = 0.20;
-iir_index2 = 0.30;
+iir_index1 = 0.10;
+iir_index2 = 0.15;
 delay_n = 5;
 change_n = 400;
 N = 50;
@@ -12,12 +12,15 @@ N = 50;
 t = (0 : dt : 20)';
 n = size(t, 1);
 
-input_seq = (t < 22) .* ((sin(t * 1) > 0.9) * 2 - 1) + (t > 25) .* sin(t - 15 - pi / 2);
+% input_seq = (t < 0) .* ((sin(t * 10) > 0.0) * 2 - 1) + (t >= 1) .* sin(t);
+input_seq = (sin(t * 5) > 0.5) * 2 - 1;
 % input_seq = (t >= 1 & t <= 3) * 1;
 % input_seq = sin(t * 2);
-output_seq = IIR_with_init(input_seq, iir_index1, 0);
+
+output_seq = IIR(input_seq, iir_index1);
+% output_seq = IIR_with_init(input_seq, iir_index1, 0);
 output_seq(change_n : end, :) = IIR_with_init(input_seq(change_n : end, :), iir_index2, output_seq(change_n - 1));
-output_seq = [zeros(delay_n + 1, 1); output_seq(1 : end - delay_n - 1, :)];
+output_seq = [zeros(delay_n + 1, 1) + output_seq(1); output_seq(1 : end - delay_n - 1, :)];
 
 m = step_response(iir_index2, n);
 simu = zeros(n, 1);
@@ -39,10 +42,11 @@ sm = model_identification(output_seq, input_seq, N, 1);
 mesh(1 : N, t, sm);
 
 ax(2) = subplot(1, 2, 2);
-plot(N : -1 : 1, sm(end, :));
+plot(1 : N, sm(end, :));
 hold on;
 plot(delay_n + 1 : N + delay_n, m(1 : N));
 hold off;
 grid on;
+xlim([1, N]);
 
 end
